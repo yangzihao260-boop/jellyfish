08b12c9f95ed0062eb6b85b15b8682ea2b8d49c3
yangzihao260-boop <yangzihao260@gmail.com>
2026-09-04 15:48:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
+  base: '/jellyfish/',
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
